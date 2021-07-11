37c9054c3df044b3b844278180bbebe6eb40270b
Anthony <anthony.pena0898@gmail.com>
2021-07-11 13:09:17 -0400

    Adds Grid View

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Anthony Pena on 7/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterImage: UIImageView!
+    
+}
